5ad2456895ebbff4032e4a454e04f5b58d4fc58d
matvey <matvey.e@mail.ru>
2024-04-26 11:22:47 +0700

    dobavili zapuck programmy v tekushem katologe dl9 do4ernego procecca

--- context_pr.c
+++ context_pr.c
@@ -3,9 +3,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char *argv[], char *envp[]) {
-         int result;
-         result = execle("/bin/cat", "/bit/cat", "args.c", 0, envp);
-         printf("erorr just try system call()\n");
-         exit(-1);
-	return 0;
+         pid_t result, procid, parentid;
+	printf("poroshdaem dochernyi procec\n");
+result = fork();
+if(result < 0) {
+printf("oshibka pri vipolnenii fork()\n");
+exit(-1);
+}
+else if(result==0) {
+procid = getpid();
+parentid = getppid();
+printf("id tekushego proccesa; %d\n", procid);
+printf("result; %d\n", parentid);
+printf("rodprocces; %d\n", result);
+execle("./args.out", " ./args.out", "args.c", 0, envp);
+printf("erorr exec()\n");
+exit(-1);
+}
+else {
+procid = getpid();
+parentid = getppid();
+printf("id tekushego proccesa; %d\n", procid);
+printf("result; %d\n", parentid);
+printf("rodprocces; %d\n", result);
+printf("erorr exec()\n");
+}
+return 0;
 }
